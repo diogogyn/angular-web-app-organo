d19209b3f8dfcba66ef079e688b860ef4fd75605
Diogo O Santos <diogogyn100@gmail.com>
2025-11-27 12:31:35 -0300

    iimplementando componentes cards

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,10 +2,11 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Cabecalho } from './componentes/cabecalho/cabecalho';
 import { Rodape } from './componentes/rodape/rodape';
+import { Livro } from './componentes/livro/livro';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Cabecalho, Rodape],
+  imports: [RouterOutlet, Cabecalho, Rodape, Livro],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
